f96dee4df5933698547d71f712ce76d2fd8ab32f
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-09-11 18:43:31 +0100

    Refactor to only import when needed

--- barcode_app/views.py
+++ barcode_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 import qrcode
-from PIL import Image
 # Create your views here.
 
 
@@ -18,19 +17,6 @@ def home(request):
 
 
 def generate_barcode(text, colour, image_choice):
-    # taking image which user wants
-    # in the QR code center
-    Logo_link = 'user.jpg'
-
-    logo = Image.open(Logo_link)
-
-    # taking base width
-    basewidth = 100
-
-    # adjust image size
-    wpercent = (basewidth/float(logo.size[0]))
-    hsize = int((float(logo.size[1])*float(wpercent)))
-    logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
     QRcode = qrcode.QRCode(
         error_correction=qrcode.constants.ERROR_CORRECT_H
     )
@@ -51,11 +37,24 @@ def generate_barcode(text, colour, image_choice):
     QRimg = QRcode.make_image(
         fill_color=QRcolor, back_color="white").convert('RGB')
 
-    # set size of QR code
-    pos = ((QRimg.size[0] - logo.size[0]) // 2,
-           (QRimg.size[1] - logo.size[1]) // 2)
-
     if image_choice == 'Yes':
+        from PIL import Image
+        # taking image which user wants
+        # in the QR code center
+        Logo_link = 'user.jpg'
+
+        logo = Image.open(Logo_link)
+
+        # taking base width
+        basewidth = 100
+
+        # adjust image size
+        wpercent = (basewidth/float(logo.size[0]))
+        hsize = int((float(logo.size[1])*float(wpercent)))
+        logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
+        # set size of QR code
+        pos = ((QRimg.size[0] - logo.size[0]) // 2,
+               (QRimg.size[1] - logo.size[1]) // 2)
         QRimg.paste(logo, pos)
 
     # save the QR code generated
